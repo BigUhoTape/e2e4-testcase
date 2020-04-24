73bb1e0615cfe1bb2ba415365d06f2af31747405
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-04-24 20:21:22 +0700

    add pagination on AllPosts page

--- src/views/AllPosts.vue
+++ src/views/AllPosts.vue
@@ -1,25 +1,57 @@
 <template>
     <div class="allPosts">
         <div class="allPosts__posts">
-            <div v-for="post in $store.state.posts" :key="post.id">
+            <div v-for="post in itemsOnPage" :key="post.id">
                 <Post :postData="post"/>
             </div>
         </div>
+        <Paginate v-model="page"
+                  :page-count="pageCount"
+                  :click-handler="changePagePagination"
+                  :prev-text="'Prev'"
+                  :next-text="'Next'"
+                  :container-class="'pagination'"
+                  :page-class="'page'"
+                  :page-link-class="'page-link'"
+                  :prev-class="'page'"
+                  :prev-link-class="'page-link opt'"
+                  :next-class="'page'"
+                  :next-link-class="'page-link opt'"
+                  :active-class="'activeClass'"
+        />
     </div>
 </template>
 
 <script>
   import Post from "../components/Post";
+  import Paginate from 'vuejs-paginate'
+  import paginationMixin from './../mixins/pagination.mixin'
 
   export default {
     name: 'AllPosts',
     components: {
-      Post
+      Post,
+      Paginate
+    },
+    mixins: [paginationMixin],
+    watch: {
+      posts() {
+        this.setupPagination(this.posts)
+      }
+    },
+    computed: {
+      posts() {
+        const posts = this.$store.state.posts;
+        return posts;
+      }
+    },
+    mounted() {
+        this.setupPagination(this.posts);
     }
   }
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
     .allPosts {
         width: 1200px;
         margin: 0 auto;
@@ -29,5 +61,37 @@
             flex-wrap: wrap;
             justify-content: space-between;
         }
+    }
+
+    .pagination {
+        display: flex;
+        justify-content: center;
+        width: 0;
+        margin: 0 auto;
+        margin-top: 30px;
+        list-style: none;
+    }
+    .page {
+        margin-right: 5px;
+    }
+    .page-link {
+        display: inline;
+        padding: 20px;
+        border: 1px solid #000;
+        border-radius: 5px;
+        font-size: 20px;
+        transition: .4s;
+        &:focus {
+            outline: none;
+        }
+        &:hover {
+            background-color: #000000;
+            color: #D2D2D2;
+            transition: .4s;
+        }
+    }
+    .opt {
+        background-color: #343434;
+        color: #D2D2D2;
     }
 </style>
