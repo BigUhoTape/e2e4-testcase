50ea53aafc8c8c9539e62b999aa070fd9ffbbe4d
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-04-24 20:27:37 +0700

    create SinglePost page

---
+++ src/views/SinglePost.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="sibglePost">{{id}}</div>
+</template>
+
+<script>
+    export default {
+      name: 'SinglePost',
+      data() {
+        return {
+          id: this.$route.params.id
+        }
+      }
+    }
+</script>
+
+<style lang="less" scoped></style>
